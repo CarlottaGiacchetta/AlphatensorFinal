d6406ff0e8d1f3c8bc1200990140fa60447f5a97
CarlottaGiacchetta <carlotta.giacchetta@outlook.it>
2025-05-04 10:42:36 +0200

    results

--- run_job2.sh
+++ run_job2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH -p long                 # Nome della coda (edu-20h)
-#SBATCH --gres=gpu:1              # Richiedi 1 GPU generica   gpu:a30.24:1
+#SBATCH --gres=gpu:a100.80:1
 #SBATCH --ntasks=1                 # Numero di task
 #SBATCH --cpus-per-task=4          # Numero di CPU per task
 #SBATCH --mem-per-cpu=7168M        # Memoria per CPU (7 GB per CPU)
---
+++ run_job4.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH -p long                 # Nome della coda (edu-20h)
+#SBATCH --gres=gpu:a100.80:1
+#SBATCH --ntasks=1                 # Numero di task
+#SBATCH --cpus-per-task=4          # Numero di CPU per task
+#SBATCH --mem-per-cpu=7168M        # Memoria per CPU (7 GB per CPU)
+#SBATCH -N 1                       # Richiedi 1 nodo
+#SBATCH -t 24:00:00                # Tempo massimo di esecuzione (20 ore)
+#SBATCH --output=outputs/output_GAT%_j.txt     # File di output (il %j inserisce il job ID)
+
+
+# Caricare CUDA (se necessario per l'uso con PyTorch o TensorFlow)
+module load CUDA/12.5.0
+# Attivare un ambiente virtuale (se presente)
+source /home/carlotta.giacchetta/.bashrc
+conda activate py11
+
+# Eseguire lo script Python
+python ~/AlphatensorFinal/parallel_main.py
